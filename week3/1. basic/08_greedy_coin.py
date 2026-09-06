9c17f835e48ea88164dde9b8f93bc4d5ce0a9ce6
Youjaeseon <wotjd0701@naver.com>
2026-09-06 20:30:43 +0900

    08 solve

--- week3/1. basic/08_greedy_coin.py
+++ week3/1. basic/08_greedy_coin.py
@@ -46,14 +46,12 @@ def make_change_greedy(change, coins):
     # TODO: 각 동전에 대해 반복
     ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
     ## 개수가 0보다 크면 결과에 추가
-    pass
-
-    for i in coins:
-        result.append(i)
-        change 
-
-    
-
+    for c in coins:
+        current_coin = change//c
+        total_coins += current_coin
+        change = change%c
+        if current_coin > 0:
+                result[c] = current_coin
     
     
     return total_coins, result
