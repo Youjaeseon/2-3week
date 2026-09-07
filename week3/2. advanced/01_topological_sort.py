368bf93fca892dfa22d4f1c8b8646263deba0596
Youjaeseon <wotjd0701@naver.com>
2026-09-07 14:57:59 +0900

    01 solve

--- week3/2. advanced/01_topological_sort.py
+++ week3/2. advanced/01_topological_sort.py
@@ -40,20 +40,32 @@ def topological_sort(vertices, edges):
         위상 정렬 순서
     """
     # TODO: 그래프와 진입 차수 초기화
-    pass
-    
-    # TODO: 그래프 구성 및 진입 차수 계산
-    pass
-    
-    # TODO: 진입 차수가 0인 정점들을 큐에 추가
-    pass
-    
+    q = deque()
+    graph = {}
+    indegree = [0]*vertices
+  
+    for i in range(vertices):
+        graph[i] = []
+
+    for u,v in edges:
+        graph[u].append(v)
+        indegree[v] += 1
+
+    for i in range(vertices):
+        if indegree[i] == 0:
+            q.append(i)
+
     result = []
-    
-    # TODO: 큐가 빌 때까지 반복
-    ## 큐에서 정점 꺼내기
-    ## 인접한 정점들의 진입 차수 감소
-    pass
+
+    while q:
+        current = q.popleft()    
+        result.append(current)
+
+        for next_node in graph[current]:
+            indegree[next_node] -= 1
+
+            if indegree[next_node] == 0:
+                q.append(next_node)
     
     return result
 
