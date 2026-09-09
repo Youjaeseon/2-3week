0ef2023e99cc7b531a11d4c0f31472fe1ac61696
Youjaeseon <wotjd0701@naver.com>
2026-09-09 20:55:22 +0900

    2차원 dp배열 초기화

--- week3/2. advanced/02_lcs.py
+++ week3/2. advanced/02_lcs.py
@@ -39,7 +39,9 @@ def lcs_length(s1: str, s2: str) -> int:
     # 빈 문자열 처리
     if not s1 or not s2:
       return 0 
-    # TODO: (len(s1)+1) x (len(s2)+1) 크기의 2차원 dp 배열을 0 으로 초기화
+    # (len(s1)+1) x (len(s2)+1) 크기의 2차원 dp 배열을 0 으로 초기화
+    dp = [[0] * (len(s2) + 1) for i in range(len(s1) + 1)]
+
     # TODO: 이중 반복문으로 점화식에 따라 dp 채우기
     # TODO: dp[len(s1)][len(s2)] 반환
     pass
