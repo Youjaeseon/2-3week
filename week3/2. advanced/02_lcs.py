eb81783a0f6b4b5ba32dc1ba8f31578d2fa438e7
Youjaeseon <wotjd0701@naver.com>
2026-09-09 20:28:54 +0900

    빈 문자열 처리

--- week3/2. advanced/02_lcs.py
+++ week3/2. advanced/02_lcs.py
@@ -36,7 +36,9 @@ def lcs_length(s1: str, s2: str) -> int:
     s1, s2 의 가장 긴 공통 부분수열의 길이를 반환.
     어느 한쪽이라도 비어 있으면 0 을 반환합니다.
     """
-    # TODO: 빈 문자열 처리
+    # 빈 문자열 처리
+    if not s1 or not s2:
+      return 0 
     # TODO: (len(s1)+1) x (len(s2)+1) 크기의 2차원 dp 배열을 0 으로 초기화
     # TODO: 이중 반복문으로 점화식에 따라 dp 채우기
     # TODO: dp[len(s1)][len(s2)] 반환
